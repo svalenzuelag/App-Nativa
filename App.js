f627af36c5823ad336a0f116fdf0ea9a3bac02ff
Saul Valenzuela <svalenzuelag@miumg.edu.gt>
2020-11-14 22:36:24 -0600

    APIFINAL

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
-import React, {useState, useEffect} from 'react'; 
-import { StyleSheet, Text, FlatList } from 'react-native';
+
+import React, {useState, useEffect,Component} from 'react'; 
+import { StyleSheet, Text, FlatList,Image, View,StatusBar, SafeAreaView} from 'react-native';
+
+
+
+
 export default function App() {
+  
+  const Appi = () => {
+    return (
+       <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#00BCD4" translucent = {true}/>
+    )
+ }
+
    const [countriesData, setCountriesData] = useState([]) 
    function fetchCountriesData() {
       fetch('https://apimedicina.azurewebsites.net/api/medicina') 
@@ -13,21 +25,31 @@ export default function App() {
     return ( 
     <FlatList 
     data={countriesData} 
-    contentContainerStyle={styles.container} 
+    contentContainerStyle={styles.container}
     keyExtractor={item => item.iD_MEDICINA} 
     renderItem={({item})=> 
     <Text style={styles.text}>
-      {item.name}
-      {item.releaseYear}
-      {item.medicina}
-      {item.existencia} 
-      {item.fechA_INGRESO}
-      {item.fechA_VENCIMIENTO} 
-      {item.imagen}
-      </Text>} 
+      <Text style={{fontSize: 25 }}>Nombre: {item.medicina}{"\n"}</Text>
+      <Text style={{fontSize: 25 }}>Existencia: {item.existencia}{"\n"}</Text>
+      <Text style={{fontSize: 25 }}>Fecha Ingreso: {item.fechA_INGRESO}{"\n"}</Text>
+      <Text style={{fontSize: 25 }}>Vencimiento:  {item.fechA_VENCIMIENTO}{"\n"}</Text>
+      <View
+      
+      >
+
+        <Image 
+          style={{ width: 100, height: 100 }}
+          source={{uri:  item.imagen}}
+        />
+      </View>
+      </Text>
+      }
+       
     /> );
    } 
    const styles = StyleSheet.create({ container: {
       paddingTop: 30, },
       text: { fontSize: 20, 
         margin: 10 }, });
+        
+        
